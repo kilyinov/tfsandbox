bd8e32b6ab405ca3e83ee64879ae734d5842754c
Konstantin Ilinov <konstantin.ilinov@gmail.com>
2026-01-20 16:56:45 +1100

    changed to create stuff in existing VPC

--- main.tf
+++ main.tf
@@ -1,30 +1,4 @@
 ######################################
-# VPC (EKS-Compatible, Lab Friendly)
-######################################
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.1.0"
-
-  name = "eks-free-tier-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs = ["ap-southeast-2a", "ap-southeast-2b", "ap-southeast-2c"]
-
-  public_subnets = [
-    "10.0.1.0/24",
-    "10.0.2.0/24"
-  ]
-
-  tags = {
-    owner = "kilinov"
-  }
-
-  enable_nat_gateway        = false
-  enable_dns_hostnames      = true
-  map_public_ip_on_launch   = true   # 👈 THIS FIXES THE NODE GROUP ERROR
-}
-
-######################################
 # EKS Cluster (Direct Resource)
 ######################################
 resource "aws_eks_cluster" "this" {
@@ -33,7 +7,7 @@ resource "aws_eks_cluster" "this" {
   version  = "1.34"
 
   vpc_config {
-    subnet_ids = module.vpc.public_subnets
+    subnet_ids = var.subnet_ids
   }
 
   # ✅ REQUIRED FOR A LABS PORTAL
@@ -66,7 +40,7 @@ resource "aws_eks_node_group" "free_tier_nodes" {
   cluster_name    = aws_eks_cluster.this.name
   node_group_name = "free-tier-nodes"
   node_role_arn  = aws_iam_role.eks_node_role.arn
-  subnet_ids     = module.vpc.public_subnets
+  subnet_ids     = var.subnet_ids
 
   instance_types = ["t3.medium"]
 
--- variables.tf
+++ variables.tf
@@ -1,3 +1,13 @@
 variable "region" {
   default = "ap-southeast-2"
 }
+
+variable "vpc_id" {
+  description = "ID of existing VPC"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs for EKS cluster and node group"
+  type        = list(string)
+}
