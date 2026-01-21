66e29fa6e1d0d5bc3b40864edc4d60648b14a7e7
Konstantin Ilinov <konstantin.ilinov@gmail.com>
2026-01-21 11:42:52 +1100

    commented out EBS CSI addon for now

--- main.tf
+++ main.tf
@@ -60,7 +60,7 @@ resource "aws_eks_node_group" "free_tier_nodes" {
   ]
 }
 
-resource "aws_eks_addon" "ebs_csi" {
-  cluster_name = aws_eks_cluster.this.name
-  addon_name   = "aws-ebs-csi-driver"
-}
+#resource "aws_eks_addon" "ebs_csi" {
+#  cluster_name = aws_eks_cluster.this.name
+#  addon_name   = "aws-ebs-csi-driver"
+#}
